5ccc087f6d24d48ad8336c2e1e5470ee1d686bc2
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2023-04-17 17:33:34 -0300

    add summatory function

--- 03-linear-regression/linear-regression.js
+++ 03-linear-regression/linear-regression.js
@@ -6,5 +6,13 @@ function quadrados(x=[]){
     return x.map((num) =>  parseFloat((parseFloat(num)**2).toFixed(4)))
 }
 
+function summatory(arr=[]){
+    return arr.reduce((sum, num)=>(sum+=num),0)
+}
+
+
+console.log(summatory([1,2,3,4,5,6,7,8,9,10]))
+console.log(summatory([1.5,2.132,3.3453]))
 console.log(quadrados([5,6.65,7.53]))
 console.log(produto([5,6.93,7.3], [1,2.42,3.3]))
+
