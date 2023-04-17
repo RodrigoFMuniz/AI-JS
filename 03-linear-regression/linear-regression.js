4305bd3bf8960326196a686335e5f98438028ac1
RodrigoFMuniz <rodrigo.muniz.dev@gmail.com>
2023-04-17 17:26:51 -0300

    Add quadrados and refactoring functions

--- 03-linear-regression/linear-regression.js
+++ 03-linear-regression/linear-regression.js
@@ -1,7 +1,10 @@
-function produto(x, y){
-    let temp = []
-    for(let i = 0; i<x.length; i++){
-        temp.push(parseFloat(x[i]) * parseFloat(y[i]))
-    }
-    return temp
+function produto(x=[], y=[]){
+    return x.map((num, index) => parseFloat((parseFloat(num) * parseFloat(y[index])).toFixed(4)))
 }
+
+function quadrados(x=[]){
+    return x.map((num) =>  parseFloat((parseFloat(num)**2).toFixed(4)))
+}
+
+console.log(quadrados([5,6.65,7.53]))
+console.log(produto([5,6.93,7.3], [1,2.42,3.3]))
